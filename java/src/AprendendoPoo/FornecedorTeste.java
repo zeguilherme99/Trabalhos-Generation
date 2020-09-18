6b98b6e47efad5be403e999a560103837f8f0051
unknown <zeguilherme008@hotmail.com>
2020-09-18 16:31:55 -0300

    Projeto

--- java/src/AprendendoPoo/FornecedorTeste.java
+++
@@ -1,9 +0,0 @@
-package AprendendoPoo;
-
-public class FornecedorTeste {
-	public static void main (String args [])
-	{
-		Fornecedor a = new Fornecedor("Cleitin","Avenida Brasil","5562-3254","Sao Paulo","Brasil",55000,33000);
-	}
-
-}
--- java/src/AprendendoPoo/PessoaTeste.java
+++
@@ -1,9 +0,0 @@
-package AprendendoPoo;
-
-public class PessoaTeste {
-	public static void main (String args[])
-	{
-		Pessoa a = new Pessoa ("Cleitin","Avenida Jabaquara", "5589-5147","Sao Paulo", "Brasil");
-		System.out.println(a.getNome());
-	}
-}
